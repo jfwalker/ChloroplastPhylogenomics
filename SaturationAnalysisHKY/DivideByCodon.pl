9209200e3797d9b89a4664235143e8b45fcb2d99
jfwalker <jfwalker@umich.edu>
2019-01-08 05:28:54 -0500

    Added the HKY saturation analysis from revision

---
+++ SaturationAnalysisHKY/DivideByCodon.pl
@@ -0,0 +1,86 @@
+$name = $ARGV[0];
+$Aname = ($name =~ /(.*?)\..*/)[0];
+open(out1, ">$name\_1");
+open(out2, ">$name\_2");
+open(out3, ">$name\_3");
+
+while($line = <>){
+
+	chomp $line;
+        if($line =~ />/){
+                print out1 "$line\n";
+		print out2 "$line\n";
+		print out3 "$line\n";
+        }else{
+                @array = split "", $line;
+                $numb = $#array / 3;
+                $k = 0; $j = 1; $m = 2;$first = "";
+		$second = ""; $third = "";
+                foreach $i (0..$numb){
+                        
+                        $first .= $array[$k];
+			$second .= $array[$j];
+			$third .= $array[$m];
+                        $k += 3;
+			$m += 3;
+			$j += 3;
+
+                }
+                print out1 "$first\n";
+		print out2 "$second\n";
+		print out3 "$third\n";
+
+        }
+}
+
+open(out, ">$name\_sat.R");
+
+print out "library(ape)\n";
+print out "pdf(file= \"/home/jfwalker/Desktop/ConflictAndChloroplasts/PreVision/FSAruns/SaturationAnalysisHKY/$Aname.pdf\")\n";
+print out "par(mfrow=c(2,2))\n";
+
+print out "setwd(\"/home/jfwalker/Desktop/ConflictAndChloroplasts/PreVision/FSAruns/SaturationAnalysisHKY/\")\n";
+print out "dat<-read.dna(file=\"$name\", format = \"fasta\", as.character=TRUE, skip=0)\n";
+print out "dat<-as.DNAbin(dat)\n";
+print out "dist<-dist.dna(dat, model=\"raw\")\n";
+print out "dist.corrected<-dist.dna(dat, model=\"F84\")\n";
+print out "plot(dist~dist.corrected, pch=20, col=\"red\", xlab=\"F84 model distance\", ylab=\"Uncorrected genetic distance\", main=\"$Aname Saturation (All Bases)\")\n";
+print out "abline(0,1, lty=2)\n";
+print out "abline(lm(dist~dist.corrected), lwd=3)\n";
+print out "lm_coef<-coef(lm(dist~dist.corrected))\n";
+print out "#text(0.1,0.05,bquote(y == .(lm_coef[2])*x))\n";
+
+print out "dat<-read.dna(file=\"$name\_1\", format = \"fasta\", as.character=TRUE, skip=0)\n";
+print out "dat<-as.DNAbin(dat)\n";
+print out "dist<-dist.dna(dat, model=\"raw\")\n";
+print out "dist.corrected<-dist.dna(dat, model=\"GG95\")\n";
+print out "plot(dist~dist.corrected, pch=20, col=\"purple\", xlab=\"F84 model distance\", ylab=\"Uncorrected genetic distance\", main=\"$Aname Saturation (1st Pos)\")\n";
+print out "abline(0,1, lty=2)\n";
+print out "abline(lm(dist~dist.corrected), lwd=3)\n";
+print out "lm_coef<-coef(lm(dist~dist.corrected))\n";
+print out "#text(0.1,0.05,bquote(y == .(lm_coef[2])*x))\n";
+
+print out "dat<-read.dna(file=\"$name\_2\", format = \"fasta\", as.character=TRUE, skip=0)\n";
+print out "dat<-as.DNAbin(dat)\n";
+print out "dist<-dist.dna(dat, model=\"raw\")\n";
+print out "dist.corrected<-dist.dna(dat, model=\"GG95\")\n";
+print out "plot(dist~dist.corrected, pch=20, col=\"blue\", xlab=\"F84 model distance\", ylab=\"Uncorrected genetic distance\", main=\"$Aname Saturation (2nd Pos)\")\n";
+print out "abline(0,1, lty=2)\n";
+print out "abline(lm(dist~dist.corrected), lwd=3)\n";
+print out "lm_coef<-coef(lm(dist~dist.corrected))\n";
+print out "#text(0.1,0.05,bquote(y == .(lm_coef[2])*x))\n";
+
+print out "dat<-read.dna(file=\"$name\_3\", format = \"fasta\", as.character=TRUE, skip=0)\n";
+print out "dat<-as.DNAbin(dat)\n";
+print out "dist<-dist.dna(dat, model=\"raw\")\n";
+print out "dist.corrected<-dist.dna(dat, model=\"GG95\")\n";
+print out "plot(dist~dist.corrected, pch=20, col=\"green\", xlab=\"F84 model distance\", ylab=\"Uncorrected genetic distance\", main=\"$Aname Saturation (3rd Pos)\")\n";
+print out "abline(0,1, lty=2)\n";
+print out "abline(lm(dist~dist.corrected), lwd=3)\n";
+print out "lm_coef<-coef(lm(dist~dist.corrected))\n";
+print out "#text(0.1,0.05,bquote(y == .(lm_coef[2])*x))\n";
+
+print out "dev.off()\n";
+
+system("Rscript $name\_sat.R");
+system("rm $name\_1 $name\_2 $name\_3 $name\_sat.R");
---
+++ SaturationAnalysisHKY/DivideByThree.pl
@@ -0,0 +1,8 @@
+while($line = <>){
+
+	if($line = />/){
+
+	}else{
+		@array = split "", $line;
+	}
+}
